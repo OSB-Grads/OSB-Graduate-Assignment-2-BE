f9c8d423d926c8e172469518464ce2c8f8fd37c9
Gattu Ganesh <gattu.ganesh@osbgroup.co.uk>
2025-09-01 18:29:22 +0530

    Closes GEN-37: Creation of Log Entity

---
+++ src/main/java/com/bank/webApplication/Entity/LogEntity.java
@@ -0,0 +1,50 @@
+package com.bank.webApplication.Entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+
+@Entity
+@Table(name="logs")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class LogEntity {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name="UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id" , updatable = false,nullable = false,columnDefinition = "VARCHAR(25)")
+    private UUID id;
+    private String user_id;
+    @Enumerated(EnumType.STRING)
+    private Action action;
+    private String details;
+    private String ip_address;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    private String timestamp;
+
+    public enum Action{
+        PROFILE_MANAGEMENT,
+        AUTHENTICATION,
+        TRANSACTIONS,
+        CREATION_MANAGEMENT
+    }
+
+    public enum Status{
+        SUCCESS,
+        FAILURE,
+        ERROR
+    }
+}
