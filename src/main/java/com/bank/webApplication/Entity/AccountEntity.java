d8a89ec82c90f9f38b55eb41dc58e9964ad0572f
AJadiyappa <desaianushadesai@gmail.com>
2025-09-01 23:44:25 +0530

    GEN-72:created the ProductEnity

--- src/main/java/com/bank/webApplication/Entity/AccountEntity.java
+++ src/main/java/com/bank/webApplication/Entity/AccountEntity.java
@@ -2,38 +2,51 @@ package com.bank.webApplication.Entity;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Table
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-
+@Table(name="accounts")
+@ToString
 public class AccountEntity {
 
 
+    @Id
+    private String account_number;
 
-    private String Account_number;
-    @Column(columnDefinition = "VARCHAR(25)")
-    private UUID id;
+    @ManyToOne
+    @JoinColumn(
+            name = "user_id",
+            referencedColumnName = "id"
+    )
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(
+            name="product_id",
+            referencedColumnName = "productId"
+    )
+    private ProductEntity product;
+
     @Enumerated(EnumType.STRING)
-    private Account_type Account_type;
-    private double Balance;
+    private account_type account_type;
+
+    private double balance;
+
     private boolean is_locked;
 
-    private String  Account_created;
-    private String  Account_updated;
+    private String  account_created;
+    private String  account_updated;
 
 
-    public enum Account_type{
+    public enum account_type{
         SAVINGS,
         FIXED_DEPOSIT
     }
