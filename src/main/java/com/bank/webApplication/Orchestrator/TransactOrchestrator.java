3c2344b2e63c79643e393bac0b300fc2c845d5a5
MChinnappa <monicaglory05@gmail.com>
2025-09-02 18:37:39 +0530

    Created TransactionService

--- src/main/java/com/bank/webApplication/Orchestrator/TransactOrchestrator.java
+++ src/main/java/com/bank/webApplication/Orchestrator/TransactOrchestrator.java
@@ -2,7 +2,9 @@ package com.bank.webApplication.Orchestrator;
 
 import com.bank.webApplication.Entity.AccountEntity;
 import com.bank.webApplication.Entity.LogEntity;
+import com.bank.webApplication.Entity.TransactionEntity;
 import com.bank.webApplication.Services.LogService;
+import com.bank.webApplication.Services.TransactionService;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,7 +32,7 @@ public class TransactOrchestrator {
             boolean depositOperation=transactionService.depositAmount(ToAccountNumber,amount);
             if(depositOperation){
                 log.info("[TransactOrchestrator ] Deposit Operation is Successful ");
-                transactionService.saveTransaction(fromAccountNumber,ToAccountNumber,amount);
+                transactionService.saveTransaction(fromAccountNumber,ToAccountNumber,amount,"Transaction Saved Successfully", TransactionEntity.type.TRANSFER, TransactionEntity.status.COMPLETED);
                 log.info("[TransactOrchestrator ] Transaction Saved Successfully");
                 logService.logintoDB(user_id, LogEntity.Action.TRANSACTIONS,"Transaction is Successful", String.valueOf(user_id), LogEntity.Status.SUCCESS);
                 return true;
