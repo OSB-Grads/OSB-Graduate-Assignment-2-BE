c6c0160a3b2653ceae2719db73e48c6dd2e0368f
Adesh Kedlaya <adeshkedlaya03@gmail.com>
2025-09-05 01:17:24 +0530

    Added AuthControllerTest

---
+++ src/test/java/com/bank/webApplication/Controller/AuthControllerTests.java
@@ -0,0 +1,56 @@
+package com.bank.webApplication.Controller;
+import com.bank.webApplication.Controllers.AuthController;
+import com.bank.webApplication.Dto.AuthDto;
+import com.bank.webApplication.Dto.JwtResponseDto;
+import com.bank.webApplication.Entity.AuthEntity;
+import com.bank.webApplication.Entity.Role;
+import com.bank.webApplication.Services.AuthService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthControllerTests {
+    @Mock
+    private AuthService authService;
+    @InjectMocks
+    private AuthController authController;
+
+    @Test
+    void testRegister(){
+        AuthDto authDto=new AuthDto();
+        authDto.setUsername("DummyUserName");
+        authDto.setPassword("DummyPassword");
+        //mock JWT Token
+        JwtResponseDto mockResponse=new JwtResponseDto("DummyToken");
+        when(authService.Signup(any(AuthDto.class))).thenReturn(mockResponse);
+        //mock call to the controller("/register")
+        ResponseEntity<JwtResponseDto> response=authController.register(authDto);
+        //assert
+        assertThat(response.getStatusCodeValue()).isEqualTo(200);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().getToken()).isEqualTo("DummyToken");
+    }
+    @Test
+    void testLogin(){
+        AuthDto authDto=new AuthDto();
+        authDto.setUsername("DummyUserName");
+        authDto.setPassword("DummyPassword");
+        //mock JWT Token
+        JwtResponseDto mockResponse=new JwtResponseDto("DummyToken");
+        when(authService.Login(authDto)).thenReturn(mockResponse);
+        //mock call to the controller("/login")
+        ResponseEntity<JwtResponseDto> response=authController.login(authDto);
+        //assert
+        assertThat(response.getStatusCodeValue()).isEqualTo(200);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().getToken()).isEqualTo("DummyToken");
+    }
+}
