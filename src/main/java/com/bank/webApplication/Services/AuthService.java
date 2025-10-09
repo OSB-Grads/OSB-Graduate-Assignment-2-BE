6b82b28b959d37a46a799b8c869367be29f63771
Adesh Kedlaya <adeshkedlaya03@gmail.com>
2025-10-09 14:02:03 +0530

    initial test commit

--- src/main/java/com/bank/webApplication/Services/AuthService.java
+++ src/main/java/com/bank/webApplication/Services/AuthService.java
@@ -58,7 +58,7 @@ public class AuthService {
         RefreshTokenEntity refreshToken = jwtUtil.generateRefreshToken(user);
         refreshTokenRepository.save(refreshToken);
         //Invoke LogService
-        logService.logintoDB(user.getId(), LogEntity.Action.AUTHENTICATION, "User Logged in Successfully", user.getUsername(), LogEntity.Status.SUCCESS);
+//        logService.logintoDB(user.getId(), LogEntity.Action.AUTHENTICATION, "User Logged in Successfully", user.getUsername(), LogEntity.Status.SUCCESS);
         log.info("[AuthService]  Login SUCCESS");
         return new JwtResponseDto(token, refreshToken.getRefreshToken());
     }
@@ -90,7 +90,7 @@ public class AuthService {
         refreshTokenRepository.save(refreshToken);
         log.info("[AuthService]  RefreshToken Saved To DataBase SUCCESS");
         //Invoke LogService
-        logService.logintoDB(user.getId(), LogEntity.Action.AUTHENTICATION, "User Signup Successfull", user.getUsername(), LogEntity.Status.SUCCESS);
+//        logService.logintoDB(user.getId(), LogEntity.Action.AUTHENTICATION, "User Signup Successfull", user.getUsername(), LogEntity.Status.SUCCESS);
         log.info("[AuthService]  SignUp SUCCESS");
         return new JwtResponseDto(token, refreshToken.getRefreshToken());
     }
@@ -115,7 +115,7 @@ public class AuthService {
         AuthEntity data = RefreshToken.getAuthEntity();
         //generates new accesstoken
         String newToken = jwtUtil.generateToken(String.valueOf(data.getId()), data.getRole().name());
-        logService.logintoDB(data.getId(), LogEntity.Action.AUTHENTICATION, "Refresh token Generated Successfully", data.getUsername(), LogEntity.Status.SUCCESS);
+//        logService.logintoDB(data.getId(), LogEntity.Action.AUTHENTICATION, "Refresh token Generated Successfully", data.getUsername(), LogEntity.Status.SUCCESS);
         log.info("[AuthService] RefreshAccessToken  SUCCESS");
         //sends back the new accesstoken along with refresh token
         return new JwtResponseDto(newToken, refreshToken);
--- src/test/java/com/bank/webApplication/Services/AuthServiceTests.java
+++ src/test/java/com/bank/webApplication/Services/AuthServiceTests.java
@@ -24,6 +24,7 @@ import org.springframework.test.context.ActiveProfiles;
 import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -38,7 +39,7 @@ public class AuthServiceTests {
     @Mock
     private PasswordHash passwordHash;
     @Mock
-    private  RefreshTokenEntity refreshTokenEntity;
+    private RefreshTokenEntity refreshTokenEntity;
 
     @Mock
     private PasswordEncoder passwordEncoder;
@@ -56,133 +57,122 @@ public class AuthServiceTests {
     private AuthEntity authEntity;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         MockitoAnnotations.openMocks(this);
-//        id=UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
-//        when(authDto.getUsername()).thenReturn("testuser");
-//        when(authDto.getPassword()).thenReturn("testpassword");
-//
-//
-//        when(authEntity.getId()).thenReturn(id);
-//        when(authEntity.getRole()).thenReturn(Role.USER);
+        UUID id = UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
+        authEntity = AuthEntity.builder()
+                .id(id)
+                .username("testuser")
+                .password("HashedPassword")
+                .role(Role.USER)
+                .build();
+        refreshTokenEntity = RefreshTokenEntity.builder()
+                .refreshToken(UUID.randomUUID().toString())
+                .authEntity(authEntity)
+                .expiry(Instant.now().plusSeconds(604800))
+                .build();
+        refreshTokenRepository.save(refreshTokenEntity);
+
+        // Repository & JWT mocks
     }
 
     //test for signup
     @Test
-    void testSignUp(){
-
-
-            id=UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
-            authEntity = AuthEntity.builder().role(Role.ADMIN).id(id).build();
-            authDto = new AuthDto();
-            authDto.setUsername("testuser");
-            authDto.setPassword("password");
-            MockedStatic<PasswordHash> mockedStaticPassword = Mockito.mockStatic(PasswordHash.class);
-            // Static method mocking
-            mockedStaticPassword.when(() -> PasswordHash.HashPass("testpassword"))
-                    .thenReturn("BcryptHashedPassword");
-            // Repository & JWT mocks
-            when(authRepository.findByUsername("testuser")).thenReturn(Optional.empty());
-            when(jwtUtil.generateToken(any(String.class), eq(Role.USER.name()) )).thenReturn("SignUpJWTtoken");
-            JwtResponseDto response = authService.Signup(authDto);
-            // Assertions
-            assertNotNull(response);
-            assertEquals("SignUpJWTtoken", response.getToken());
+    void testSignUp() {
+        authDto = new AuthDto();
+        authDto.setUsername(authEntity.getUsername());
+        authDto.setPassword(authEntity.getPassword());
+        MockedStatic<PasswordHash> mockedStaticPassword = Mockito.mockStatic(PasswordHash.class);
+        // Static method mocking
+//             Repository & JWT mocks
+        when(authRepository.findByUsername("testuser")).thenReturn(Optional.empty());
+        mockedStaticPassword.when(() -> PasswordHash.HashPass("testpassword"))
+                .thenReturn("HashedPassword");
+        JwtResponseDto response = authService.Signup(authDto);
+        // Assertions
+        assertNotNull(response);
+        assertNotNull(response.getToken());
+        assertNotNull(response.getRefreshToken());
 
 
     }
+
     @Test
     void testSignUp_UserAlreadyExists() {
-
-
-            UUID id = UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
-            authEntity = AuthEntity.builder()
-                    .id(id)
-                    .username("testuser")
-                    .password("existingPassword")
-                    .role(Role.USER)
-                    .build();
-            authDto = new AuthDto();
-            authDto.setUsername("testuser");
-            authDto.setPassword("password");
-            // Mock repository to return existing user
-            when(authRepository.findByUsername("testuser")).thenReturn(Optional.of(authEntity));
-            // Assert
-            Exception exception = assertThrows(RuntimeException.class, () -> {
-                authService.Signup(authDto);
-            });
-            //  verify exception message
-            assertEquals("User Already Exist", exception.getMessage());
-
+        authDto = new AuthDto();
+        authDto.setUsername("testuser");
+        authDto.setPassword("password");
+        // Mock repository to return existing user
+        when(authRepository.findByUsername("testuser")).thenReturn(Optional.of(authEntity));
+        // Assert
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            authService.Signup(authDto);
+        });
+        //  verify exception message
+        assertEquals("User Already Exist", exception.getMessage());
     }
     @Test
-    void testLogin(){
-
-            UUID id=UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
-            authEntity=AuthEntity.builder()
-                    .id(id)
-                    .username("testuser")
-                    .password("HashedPassword")
-                    .role(Role.USER)
-                    .build();
-            authDto = new AuthDto();
-            authDto.setUsername("testuser");
-            authDto.setPassword("PlainPassword");
-            when(passwordEncoder.matches("PlainPassword","HashedPassword")).thenReturn(true);
-            when(authRepository.findByUsername("testuser")).thenReturn(Optional.of(authEntity));
-            when(jwtUtil.generateToken(any(String.class), eq(Role.USER.name()) )).thenReturn("LoginJwtToken");
-            JwtResponseDto response=authService.Login(authDto);
-            assertNotNull(response);
-            assertEquals("LoginJwtToken",response.getToken());
-
+    void testLogin() {
+        authDto = new AuthDto();
+        authDto.setUsername("testuser");
+        authDto.setPassword("PlainPassword");
+        when(passwordEncoder.matches("PlainPassword", "HashedPassword")).thenReturn(true);
+        when(authRepository.findByUsername("testuser")).thenReturn(Optional.of(authEntity));
+        when(jwtUtil.generateToken(any(String.class), eq(Role.USER.name()))).thenReturn("LoginJwtToken");
+        when(jwtUtil.generateRefreshToken(authEntity)).thenReturn(refreshTokenEntity);
+        JwtResponseDto response = authService.Login(authDto);
+        assertNotNull(response);
+        assertEquals("LoginJwtToken", response.getToken());
+        assertNotNull(response.getRefreshToken());
     }
+
     @Test
-    void testLogin_UserNotfound(){
-        authDto=new AuthDto();
+    void testLogin_UserNotfound() {
+        authDto = new AuthDto();
         authDto.setUsername("testuser");
         authDto.setPassword("plainpassword");
 
         when(authRepository.findByUsername("testuser")).thenReturn(Optional.empty());
 
-        Exception e=assertThrows(RuntimeException.class,()->{
+        Exception e = assertThrows(RuntimeException.class, () -> {
             authService.Login(authDto);
         });
-        assertEquals("Invalid UserName or User not found",e.getMessage());
+        assertEquals("Invalid UserName or User not found", e.getMessage());
     }
+
     @Test
-    void testLogin_InvalidPassword(){
-        authDto=new AuthDto();
+    void testLogin_InvalidPassword() {
+        authDto = new AuthDto();
         authDto.setUsername("testuser");
         authDto.setPassword("invalidpassword");
-        authEntity=AuthEntity.builder()
-                .id(id)
-                .username("testuser")
-                .password("correctHashedPassword")
-                .role(Role.USER)
-                .build();
         when(authRepository.findByUsername("testuser")).thenReturn(Optional.of(authEntity));
-        when(passwordEncoder.matches("invalidpassword","correctHashedPassword")).thenReturn(false);
-        Exception e=assertThrows(RuntimeException.class,()->{
+        when(passwordEncoder.matches("invalidpassword", "HashedPassword")).thenReturn(false);
+        Exception e = assertThrows(RuntimeException.class, () -> {
             authService.Login(authDto);
         });
-        assertEquals("Invalid PassWord",e.getMessage());
+        assertEquals("Invalid PassWord", e.getMessage());
     }
+
     @Test
-    void  LogOut(){
-        UUID id = UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
-        authEntity = AuthEntity.builder()
-                .id(id)
-                .username("testuser")
-                .password("existingPassword")
-                .role(Role.USER)
-                .build();
-        RefreshTokenEntity Refreshtoken = jwtUtil.generateRefreshToken(authEntity);
-        var claims= RefreshTokenEntity.builder()
-                .refreshToken("SampleRefreshToken")
-                .authEntity(authEntity)
-                .expiry(Instant.now().plusSeconds(1000 * 60*2))//expiry time of 1 day
-//        1 * 24 * 60 * 60
-                .build();
-        when(refreshTokenRepository.deleteByRefreshToken("SampleRefreshToken")).thenReturn(1);
+    void LogOut() {
+
+        when(refreshTokenRepository.deleteByRefreshToken(refreshTokenEntity.getRefreshToken())).thenReturn(1);
+        authService.LogOut(refreshTokenEntity.getRefreshToken());
+    }
+
+    @Test
+    void RefreshAccessToken_shouldProvideNewToken() {
+        when(refreshTokenRepository.findByRefreshToken(refreshTokenEntity.getRefreshToken())).thenReturn(Optional.of(refreshTokenEntity));
+        when(jwtUtil.generateToken(authEntity.getId().toString(), authEntity.getRole().toString())).thenReturn("new-jwt-access-token");
+        JwtResponseDto response = authService.RefreshAccessToken(refreshTokenEntity.getRefreshToken());
+        assertEquals("new-jwt-access-token", response.getToken());
+        assertEquals(refreshTokenEntity.getRefreshToken(), response.getRefreshToken());
+    }
+
+    @Test
+    void RefreshAccessToken_tokenexpired() {
+        refreshTokenEntity.setExpiry(Instant.now().minusSeconds(10));
+        when(refreshTokenRepository.findByRefreshToken(refreshTokenEntity.getRefreshToken())).thenReturn(Optional.of(refreshTokenEntity));
+        assertThrows(RuntimeException.class, () -> authService.RefreshAccessToken(refreshTokenEntity.getRefreshToken()));
     }
 }
