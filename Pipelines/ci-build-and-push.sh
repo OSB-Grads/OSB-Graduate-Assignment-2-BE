1e265db1f9e2e05d38e78786cd42fccffb57d0d2
MChinnappa <monicaglory05@gmail.com>
2025-10-23 01:03:01 +0530

    Updated CI file

--- Pipelines/ci-build-and-push.sh
+++ Pipelines/ci-build-and-push.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
+# Exit on any error and print all commands
 set -e
+set -o pipefail
+set -x
 
-# Arguments passed from pipeline
+# Function to log messages and flush immediately
+log() {
+  echo "$1"
+  sleep 0.1
+}
+
+# Arguments from pipeline
 ACR_NAME=$1
 IMAGE_NAME=$2
 DOCKERFILE_PATH=$3
 IMAGE_TAG=$4
 
-echo " Logging into ACR..."
+log "-------------------------------------------"
+log "Starting CI Build & Push Docker Image"
+log "ACR: $ACR_NAME"
+log "Image: $IMAGE_NAME"
+log "Dockerfile: $DOCKERFILE_PATH"
+log "Tag: $IMAGE_TAG"
+log "-------------------------------------------"
+
+log "Logging into ACR..."
 az acr login --name $ACR_NAME
 
-echo " Building Docker image..."
+log "Building Docker image..."
 docker build -t $ACR_NAME.azurecr.io/$IMAGE_NAME:$IMAGE_TAG -f $DOCKERFILE_PATH .
 
-echo " Pushing Docker image to ACR..."
+log "Pushing Docker image to ACR..."
 docker push $ACR_NAME.azurecr.io/$IMAGE_NAME:$IMAGE_TAG
 
-echo " CI Build & Push Complete!"
+log "CI Build & Push Complete!"
+log "-------------------------------------------"
